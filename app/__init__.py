eb6e8818cc64ad6470a96191290a909c57fee542
deepakGit00 <deepak00@gmail.com>
2026-05-22 23:27:44 -0400

    Initialize Flask app with database and login manager

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_login import LoginManager
+from app.models import db, User
+
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///karaoke.db'
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from app.routes import main
+    app.register_blueprint(main)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
